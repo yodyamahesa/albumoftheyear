2b9c745199d092c43ca493555c2c1cf9fcb89d86
Yodya Mahesa <yodyamahesa123@gmail.com>
2024-05-05 17:23:09 +0800

    Add design + processing + compress

--- 03-aoty-visualization/myproject/manage.py
+++ 03-aoty-visualization/myproject/manage.py
@@ -9,6 +9,11 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
     try:
         from django.core.management import execute_from_command_line
+
+        # Override default port for `runserver` command
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = "80"
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
--- 03-aoty-visualization/myproject/myapp/views.py
+++ 03-aoty-visualization/myproject/myapp/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+import pandas as pd
+dfalbum = pd.read_csv('myapp/dataset/albums.csv', sep=';')
+dfrating = pd.read_csv('myapp/dataset/albums.csv', sep=';')
+artists = dfalbum['artis']
+
 def index(request):
-    return render(request, "myapp/index.html")
+    return render(request, "myapp/index.html", {
+        "artists": artists
+    })
