806919c59652e7bdcca5851c238b4ff2c9750bc2
Mirah Wardhana <mirah.indira@gmail.com>
2024-05-26 10:27:51 +0800

    perbaiki responsive dan tambah dummy page (sampah) buat nyimpen array link

--- 03-aoty-visualization/myproject/myapp/views.py
+++ 03-aoty-visualization/myproject/myapp/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-
-
-# Create your views here.
-
 import pandas as pd
-dfalbum = pd.read_csv('myapp/dataset/albums.csv', sep=';')
-dfrating = pd.read_csv('myapp/dataset/albums.csv', sep=';')
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+df_album = pd.read_csv('myapp/dataset/albums.csv', sep=';')
 
 album = []
-for index, row in dfalbum.iterrows():
+for index, row in df_album.iterrows():
     album.append((row['album'], row['artis'], row['thumbnail_album']))
+
+
 
 def index(request):
     return render(request, "myapp/index.html", {
-        "album_atas": album[0:5],
-        "album_bawah": album[10:15]
+        "albums": album[0:100]
+    })
+    
+    
+def funsampah(request):
+    return render(request, "myapp/sampah.html", {
+        "albums": album[0:100]
     })
